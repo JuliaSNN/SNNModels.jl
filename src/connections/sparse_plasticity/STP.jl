db2430ee06865f92c130571a6b2553d2909cbf5e
Alessio Quaresima <aquaresi@pasteur.fr>
2025-08-30 23:31:20 +0200

    added doc to STP

--- src/connections/sparse_plasticity/STP.jl
+++ src/connections/sparse_plasticity/STP.jl
@@ -1,3 +1,23 @@
+"""
+    MarkramSTPParameter{FT <: AbstractFloat} <: STPParameter
+
+    The model is based on refractoriness of the synaptic release process, which can be rephrased by stating that:
+    The fraction (U) of the synaptic efficacy used by an AP becomes instantaneously unavailable for subsequent use and recovers with a time constant of τD (τrec, depression). The fraction of available synaptic efficacy is termed `x.`  A facilitating mechanism is included in the model as a pulsed increase in U by each AP. The running value of U is referred to as u and U remains a parameter that applies to the first AP in a train. u decays with a single exponential, τF (facilitation), to its resting value U. The amount of synaptic efficacy enhanced by a action potential is assumed to be U(1-u).
+    The increase in the amplitude of the postsynaptic response is proportional to the product of u and x.
+
+    The actual implementation follows the equations described in Mongillo et al. (2008) for clarity.
+
+# Fields
+- `τD::FT`: Time constant for depression (default: 200ms)
+- `τF::FT`: Time constant for facilitation (default: 1500ms)
+- `U::FT`: Maximum utilization of synaptic resources (default: 0.2)
+- `Wmax::FT`: Maximum synaptic weight (default: 1.0pF)
+- `Wmin::FT`: Minimum synaptic weight (default: 0.0pF)
+
+This struct is used to configure the short-term plasticity dynamics in synaptic connections
+following the model described by Markram et al. (1998).
+"""
+MarkramSTPParameter
 @snn_kw struct MarkramSTPParameter{FT = Float32} <: STPParameter
     τD::FT = 200ms # τx
     τF::FT = 1500ms # τu
