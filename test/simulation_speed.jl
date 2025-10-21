a8ca5e3de210f2f3600e25ff2b87a63a396e1ada
Alessio <alessio.quaresima@pasteur.fr>
2025-10-21 21:43:32 +0200

    simulation speed

--- test/simulation_speed.jl
+++ test/simulation_speed.jl
@@ -37,11 +37,11 @@ model = compose(; E, stim_exc, stim_inh, silent=true)
 using SNNModels
 @load_units
 
-E = Tripod()
+E = SNNModels.Tripod(N=1)
 
 poisson_exc = PoissonLayer(
     10.2Hz,    # Mean firing rate (Hz) 
-    N = 1000, # Neurons in the Poisson Layer
+    N = 200, # Neurons in the Poisson Layer
 )
 poisson_inh = PoissonLayer(
     3Hz,       # Mean firing rate (Hz)
@@ -51,18 +51,27 @@ poisson_inh = PoissonLayer(
 exc_conn = 
 (
     p = 1.0f0,  # Probability of connecting to a neuron
-    μ = 1.0,  # Synaptic strength (nS)
+    μ = 10.0,  # Synaptic strength (nS)
 )
 inh_conn = ( 
     p = 1.0f0,   # Probability of connecting to a neuron
     μ = 4.0,   # Synaptic strength (nS)
 )
 # Create the Poisson layers for excitatory and inhibitory inputs
-stim_exc = Stimulus(poisson_exc, E, :glu, :d1, conn=exc_conn, name = "noiseE")
-stim_inh = Stimulus(poisson_inh, E, :gaba, :d1, conn=inh_conn, name = "noiseI")
+stim_exc1 = Stimulus(poisson_exc, E, :glu, :d1, conn=exc_conn, name = "noiseE")
+stim_inh1 = Stimulus(poisson_inh, E, :gaba, :d1, conn=inh_conn, name = "noiseI")
+stim_exc2 = Stimulus(poisson_exc, E, :glu, :d2, conn=exc_conn, name = "noiseE")
+stim_inh2 = Stimulus(poisson_inh, E, :gaba, :d2, conn=inh_conn, name = "noiseI")
 
+model = compose(; E, stim_exc1, stim_inh1, stim_exc2, stim_inh2, silent=true)
+SNN.monitor!(E, [:fire, :v_d1, :v_s, :v_d2])
+sim!(model, 5s)
+SNN.raster(model.pop,    [4s, 5s])
+SNN.vecplot(E, :v_d1, neurons=1, r=1s:5s)
+SNN.vecplot(E, :v_d2, neurons=1, r=1s:5s)
+SNN.vecplot(E, :v_s, neurons=1, r=1s:5s)
+##
 
-model = compose(; E, stim_exc, stim_inh, silent=true)
 @profview sim!(model, 50s)
 @btime sim!(model, 10s)
 #   735.852 ms (799501 allocations: 15.86 MiB) on DellTower
