a2c07ee98bb5d04c74a5573eb6997f16dbd570d9
Alessio <alessio.quaresima@pasteur.fr>
2026-02-13 15:41:07 +0100

    added docs in rate.jl

--- src/populations/rate.jl
+++ src/populations/rate.jl
@@ -1,4 +1,43 @@
+"""
+    RateParameter <: AbstractPopulationParameter
+
+Parameters for rate-based neuron model. This model has no adjustable parameters - it uses a simple tanh transfer function.
+"""
+RateParameter
+
 struct RateParameter <: AbstractPopulationParameter end
+
+"""
+    Rate{VFT} <: AbstractPopulation
+
+Rate-based neuron population model with tanh activation function.
+
+# Fields
+## Population Info
+- `id::String`: Unique identifier (default: random 12-character string)
+- `name::String`: Population name (default: "Rate")
+- `N::Int32`: Number of neurons (default: 100)
+
+## Parameters
+- `param::RateParameter`: Model parameters (no adjustable parameters)
+
+## State Variables
+- `x::VFT`: Internal state/activation variable (initialized with random values)
+- `r::VFT`: Output firing rate, r = tanh(x) (initialized from x)
+- `g::VFT`: Synaptic input (default: zeros)
+- `I::VFT`: External input current (default: zeros)
+
+## Recordings
+- `records::Dict`: Dictionary for storing simulation data
+
+# Model Equations
+- dx/dt = -x + g + I
+- r = tanh(x)
+
+# References
+- [Neuronal Dynamics - Rate Models](https://neuronaldynamics.epfl.ch/online/Ch15.S3.html)
+"""
+Rate 
 
 @snn_kw mutable struct Rate{VFT = Vector{Float32}} <: AbstractPopulation
     id::String = randstring(12)
@@ -26,10 +65,20 @@ function synaptic_target(
 end
 
 """
-[Rate Neuron](https://neuronaldynamics.epfl.ch/online/Ch15.S3.html)
-"""
-Rate
+    integrate!(p::Rate, param::RateParameter, dt::Float32)
 
+Update rate-based neuron population for one timestep.
+
+# Arguments
+- `p::Rate`: The neuron population
+- `param::RateParameter`: Model parameters (unused)
+- `dt::Float32`: Time step size
+
+# Details
+- Updates internal state x: dx/dt = -x + g + I
+- Computes output rate: r = tanh(x)
+- Rate r is bounded between -1 and 1
+"""
 function integrate!(p::Rate, param::RateParameter, dt::Float32)
     @unpack N, x, r, g, I = p
     @inbounds for i = 1:N
