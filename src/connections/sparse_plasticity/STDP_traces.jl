284cda611ee6c82a824c073fef07042b031cef90
Alessio <alessio.quaresima@pasteur.fr>
2026-02-16 18:12:13 +0100

    Confavreux and Gerstner STDP with event-based plasticity

--- src/connections/sparse_plasticity/STDP_traces.jl
+++ src/connections/sparse_plasticity/STDP_traces.jl
@@ -12,6 +12,18 @@ STDPGerstner
     Wmin::FT = 0.0pF                  # Min weight (negative for inhibition)
 end
 
+@snn_kw struct STDPConfavreux2025{FT = Float32} <: STDPParameter
+    η::FT = 0.001
+    α::FT = 0 ## baseline rate dependency post
+    β::FT = 0 ## baseline rate dependency pre
+    κ::FT = 1.0f0 # stdp pre->post
+    γ::FT = 1.0f0 # stdp post->pre
+    τpre::FT = 20ms            
+    τpost::FT = 20ms      
+    Wmin::FT = 0.0pF
+    Wmax::FT = 30.0pF
+end
+
 @doc """
     STDPMexicanHat{FT = Float32}
     
@@ -30,14 +42,17 @@ STDPMexicanHat
     Wmin::FT = 0.0pF               # Min weight (negative for inhibition)
 end
 
+## Common variables for STDP rules
 # STDP Variables Structure
 @snn_kw struct STDPVariables{VFT = Vector{Float32},IT = Int} <: LTPVariables
     Npost::IT                      # Number of post-synaptic neurons
     Npre::IT                       # Number of pre-synaptic neurons
-    tpre::VFT = zeros(Npre)           # Pre-synaptic spike trace
-    tpost::VFT = zeros(Npost)          # Post-synaptic spike trace
-    last_pre::VFT = zeros(Npre)          # Last pre-synaptic spike time
-    last_post::VFT = zeros(Npost)         # Last post-synaptic spike
+    tpre::VFT = zeros(Float32, Npre)           # Pre-synaptic spike trace
+    tpost::VFT = zeros(Float32, Npost)          # Post-synaptic spike trace
+    last_pre::VFT = zeros(Float32, Npre)          # Last pre-synaptic spike time
+    last_post::VFT = zeros(Float32,Npost)         # Last post-synaptic spike
+    Δpre::VFT = zeros(Float32, length(tpre))
+    Δpost::VFT = zeros(Float32, length(tpost))
     active::VBT = [true]
 end
 
@@ -45,6 +60,8 @@ end
 function plasticityvariables(param::T, Npre, Npost) where {T<:STDPParameter}
     return STDPVariables(Npre = Npre, Npost = Npost)
 end
+
+##
 
 
 # Function to implement STDP update rule
@@ -56,75 +73,108 @@ function plasticity!(
     T::Time,
 ) where {PT<:AbstractSparseSynapse}
     @unpack rowptr, colptr, I, J, index, W, fireJ, fireI, g, index = c
-    @unpack tpre, tpost, last_pre, last_post = variables
+    @unpack tpre, tpost, last_pre, last_post, Δpre, Δpost = variables
     @unpack A_pre, A_post, τpre, τpost, Wmax, Wmin = param
 
 
     # Update weights based on pre-post spike timing
     @inbounds @fastmath begin
         t = get_time(T)
-        last_pre[findall(fireJ)] .= t
-        last_post[findall(fireI)] .= t
-
-        Δ_pre = exp.(-(t .- last_pre) ./ τpre)
-        Δ_post = exp.(-(t .- last_post) ./ τpost)
-        Δ_pre[Δ_pre .> 1f0] .= 0
-        Δ_post[Δ_post .> 1f0] .= 0
-
-        # @turbo 
-        @simd for s in eachindex(W)
-            i, j = I[s], J[s]
+        @simd for j in eachindex(fireJ)
             if fireJ[j]
-                W[s] +=  A_pre * Δ_pre[i] # post-pre
+                tpre[j] = tpre[j] * exp(-(t - last_pre[j]) / τpre) + A_pre
+                last_pre[j] = t
             end
-            if fireI[i] 
-                W[s] += A_post * Δ_post[j]
+            Δpre[j] = t > last_pre[j] ? tpre[j] * exp(-(t - last_pre[j]) / τpre) : 0f0
+        end
+        @simd for i in eachindex(fireI)
+            if fireI[i]
+                tpost[i] = tpost[i] * exp(-(t - last_post[i]) / τpost) + A_post
+                last_post[i] = t
             end
-            # W[s] = clamp(W[i], Wmin, Wmax)
-         end
+            Δpost[i] = t > last_post[i] ? tpost[i] * exp(-(t - last_post[i]) / τpost) : 0f0
+        end
 
-        # for i in eachindex(findall(fireI)) # loop over post-synaptic neurons
-        #     for st = rowptr[i]:(rowptr[i+1]-1)
-        #         s = index[st]
-        #         if my_fireJ[st]
-        #             W[s] += Δ * A_post  # pre-post
-        #             # W[s] = clamp(W[s], Wmin, Wmax)
-        #         end
-        #     end
-        # end
-
-        # # Update weights based on pre-post spike timing
-        # for j in eachindex(fireJ) # loop over pre-synaptic neurons
-        #     Δ = exp(-( t - last_pre[j])/τpre)
-        #     Δ > 1f0 && (Δ = 0)
-        #     @simd for s = colptr[j]:(colptr[j+1]-1)
-        #         if fireI[I[s]]
-        #             W[s] += Δ * A_pre  # pre-post
-        #             # W[s] = clamp(W[s], Wmin, Wmax)
-        #         end
-        #     end
-        # end
-        # @turbo for i in eachindex(fireI)
-        #     tpost[i] += dt * (-tpost[i]) / τpost
-        # end
-        # @simd for i in findall(fireI)
-        #     tpost[i] += A_post
-        # end
-
-        # @turbo for j in eachindex(fireJ)
-        #     tpre[j] += dt * (-tpre[j]) / τpre
-        # end
-        # @simd for j in findall(fireJ)
-        #     tpre[j] += A_pre
-        # end
-
+        chunks = Iterators.partition(eachindex(W), cld(length(W), Threads.nthreads())) |> collect
+        Threads.@threads for c in eachindex(chunks) # Iterate over presynaptic neurons
+            for s in chunks[c]
+                i, j = I[s], J[s] # post and pre neuron indices
+                if fireI[i] # post spike
+                    W[s] +=  A_pre * Δpre[j] # post-pre
+                end
+                if fireJ[j] # pre spike
+                    W[s] += A_post * Δpost[i] # pre-post
+                end
+                W[s] = clamp(W[s], Wmin, Wmax)
+            end
+        end
     end
-    # Clamp weights to the specified bounds
-    # @turbo for i in eachindex(W)
-    #     @inbounds W[i] = clamp(W[i], Wmin, Wmax)
-    # end
 end
 
+# Function to implement STDP update rule
+function plasticity!(
+    c::PT,
+    param::STDPConfavreux2025,
+    variables::STDPVariables,
+    dt::Float32,
+    T::Time,
+) where {PT<:AbstractSparseSynapse}
+    @unpack rowptr, colptr, I, J, index, W, fireJ, fireI, g, index = c
+    @unpack tpre, tpost, last_pre, last_post, Δpre, Δpost = variables
+    @unpack η, α, β, κ, γ, τpre, τpost, Wmin, Wmax = param
+
+    # Update weights based on pre-post spike timing
+    # @inbounds 
+    @fastmath begin
+        t = get_time(T)
+        @simd for j in eachindex(fireJ)
+            if fireJ[j]
+                tpre[j] = tpre[j] * exp(-(t - last_pre[j]) / τpre) + 1f0
+                last_pre[j] = t
+            end
+            Δpre[j] = t > last_pre[j] ? tpre[j] * exp(-(t - last_pre[j]) / τpre) : 0f0
+        end
+        @simd for i in eachindex(fireI)
+            if fireI[i]
+                tpost[i] = tpost[i] * exp(-(t - last_post[i]) / τpost) + 1f0
+                last_post[i] = t
+            end
+            Δpost[i] = t > last_post[i] ? tpost[i] * exp(-(t - last_post[i]) / τpost) : 0f0
+        end
+
+        # @simd for i = 1:(length(rowptr)-1)
+        #     @simd for st = rowptr[i]:(rowptr[i+1]-1)
+        #         s = index[st]
+        #         if fireJ[J[s]] 
+        #             W[s] += η * (κ * Δpost[i] + α) # pre-post
+        #             W[s] = clamp(W[s], Wmin, Wmax)
+        #         end
+        #     end
+        # end
+        # @simd for j = 1:(length(colptr)-1)
+        #     @simd for s = colptr[j]:(colptr[j+1]-1)
+        #         if fireI[I[s]] 
+        #             W[s] +=  η * (γ * Δpre[j] + β) # post-pre
+        #             W[s] = clamp(W[s], Wmin, Wmax)
+        #         end
+        #     end
+        # end
+
+        chunks = Iterators.partition(eachindex(W), cld(length(W), Threads.nthreads())) |> collect
+        Threads.@threads for c in eachindex(chunks) # Iterate over presynaptic neurons
+            for s in chunks[c]
+                i, j = I[s], J[s] # post and pre neuron indices
+                if fireI[i] # post spike
+                    W[s] +=  η * (γ * Δpre[j] + β) # post-pre
+                end
+                if fireJ[j] # pre spike
+                    W[s] += η * (κ * Δpost[i] + α) # pre-post
+                end
+                W[s] = clamp(W[s], Wmin, Wmax)
+            end
+        end
+    end
+end
 
 MexicanHat(x::Float32) = (1 - x) * exp(-x / sqrt(2)) |> x -> isnan(x) ? 0 : x
 function plasticity!(
@@ -182,4 +232,4 @@ function plasticity!(
 end
 
 # Export the relevant functions and structs
-export STDPVariables, plasticityvariables, plasticity!, STDPMexicanHat, STDPGerstner
+export STDPVariables, plasticityvariables, plasticity!, STDPMexicanHat, STDPGerstner, STDPConfavreux2025
